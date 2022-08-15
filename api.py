6f06e75f4d1f41c735e155a15103e5cc78790a38
Maya <mail@hyakosm.net>
2022-08-15 21:32:28 +0200

    docker-compose

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv('allow_origins')],
+    allow_origins=[os.getenv('ALLOW_ORIGINS')],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
